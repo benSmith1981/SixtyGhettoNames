84f3750ff51a82022c000d3069d8a3a984fd8f15
benSmith1981 <bensmith@Bens-MacBook-Air.local>
2013-10-04 15:33:48 +0100

    First

---
+++ SixtyGhettoNames/SixtyGhettoNames/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SixtyGhettoNames
+//
+//  Created by Ben Smith on 04/10/2013.
+//  Copyright (c) 2013 Ben Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SixtyGhettoNames/SixtyGhettoNames/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SixtyGhettoNames
+//
+//  Created by Ben Smith on 04/10/2013.
+//  Copyright (c) 2013 Ben Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
